37e7ef296291f216b0bb98fc9a9ca97b702656e2
Alvin Okafor <alvin2k99@gmail.com>
2022-12-08 17:24:07 +0100

    tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 function App() {
   return (
-    <div>
+    <div className="mx-auto text-center">
       <h1>Merce Store</h1>
+      {/* <div>
+      <button className="peer w-[150px] h-[50px] border-2 bg-slate-900 text-white mx-auto">
+        Open Dropdown
+      </button>
+      <div className="pt-5 scale-0 opacity-0 w-[150px] mx-auto peer-hover:scale-100 peer-hover:opacity-100 peer-hover:transition-all peer-hover:duration-150 hover:scale-100 hover:opacity-100">
+        <ul className="bg-green-500 space-y-5 py-4">
+          <li className="list-item">Dropdown Item</li>
+          <li className="list-item">Dropdown Item</li>
+          <li className="list-item">Dropdown Item</li>
+          <li className="list-item">Dropdown Item</li>
+        </ul>
+      </div>
+      </div> */}
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,22 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Inter', 'san-serif']
-      }
+      },
+      colors: {
+        'bud-green': '#83BF6E',
+        'lavender-blue': '#CABDFF',
+        'uranian-blue': '#B1E5FC',
+        'peach': '#FFBC99',
+        'azure-blue': '#2A85FF',
+        'sonic-silver': '#6F767E',
+        'gunmetal': '#272B30',
+        'black': '#111315',
+        'soft-black': '#1A1D1F'
+      },
+      fontSize: {
+        "heading": "2.5rem",
+        "mobile-heading": "2rem",
+      },
     },
   },
   plugins: [],

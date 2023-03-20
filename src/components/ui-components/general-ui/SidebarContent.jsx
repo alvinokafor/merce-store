59a9e45f984e2db570883433e0c7a357fe6c6c24
Alvin Okafor <alvin2k99@gmail.com>
2023-03-20 23:42:10 +0100

    minor ui update

--- src/components/ui-components/general-ui/SidebarContent.jsx
+++ src/components/ui-components/general-ui/SidebarContent.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
+export default function SidebarContent({ setMobileMenuOpen, currentPath }) {
   const [productSubMenu, setProductSubMenu] = useState(false);
   const [customerSubMenu, setCustomerSubMenu] = useState(false);
   const [incomeSubMenu, setIncomeSubMenu] = useState(false);
@@ -23,15 +23,18 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
 
   //closes mobile menu on page switch
   const changeLinkHandler = () => {
-    setMobileMenuOpen(false)
-  }
+    setMobileMenuOpen(false);
+  };
 
   return (
     <div>
       <div className="px-6 font-semibold space-y-2 text-sonic-silver mt-6">
-
-        
-        <div id="menu-item" className={currentPath === '' ? "menu-item-active group" : 'menu-item group'}>
+        <div
+          id="menu-item"
+          className={
+            currentPath === "" ? "menu-item-active group" : "menu-item group"
+          }
+        >
           <svg
             width="24"
             height="24"
@@ -40,22 +43,36 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              className={currentPath === '' ? 'fill-white' : "group-hover:fill-white"}
+              className={
+                currentPath === "" ? "fill-white" : "group-hover:fill-white"
+              }
               fillRule="evenodd"
               clipRule="evenodd"
               d="M5 22C3.34315 22 2 20.6569 2 19V11.3361C2 10.4857 2.36096 9.67518 2.99311 9.10625L9.9931 2.80625C11.134 1.77943 12.866 1.77943 14.0069 2.80625L21.0069 9.10625C21.639 9.67518 22 10.4857 22 11.3361V19C22 20.6569 20.6569 22 19 22H5ZM20 11.3361V19C20 19.5523 19.5523 20 19 20H16V15C16 13.3432 14.6569 12 13 12H11C9.34315 12 8 13.3432 8 15V20H5C4.44772 20 4 19.5523 4 19V11.3361C4 11.0526 4.12032 10.7825 4.33104 10.5928L11.331 4.29284C11.7113 3.95056 12.2887 3.95056 12.669 4.29284L19.669 10.5928C19.8797 10.7825 20 11.0526 20 11.3361ZM10 20V15C10 14.4478 10.4477 14 11 14H13C13.5523 14 14 14.4478 14 15V20H10Z"
               fill="#6F767E"
             />
           </svg>
-          <Link to='/'>
-          <p onClick={changeLinkHandler} className={currentPath === '' ? "text-white" : "group-hover:text-white"}>Home</p>
+          <Link to="/">
+            <p
+              onClick={changeLinkHandler}
+              className={
+                currentPath === "" ? "text-white" : "group-hover:text-white"
+              }
+            >
+              Home
+            </p>
           </Link>
         </div>
-        
 
         <aside>
-          
-          <div id="menu-item" className={currentPath === 'products' ? "menu-item-active group" : 'menu-item group'}>
+          <div
+            id="menu-item"
+            className={
+              currentPath === "products"
+                ? "menu-item-active group"
+                : "menu-item group"
+            }
+          >
             <svg
               width="24"
               height="24"
@@ -64,7 +81,11 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                className={currentPath === 'products' ? 'fill-white' : "group-hover:fill-white"}
+                className={
+                  currentPath === "products"
+                    ? "fill-white"
+                    : "group-hover:fill-white"
+                }
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M22.1597 7.1842L19.2326 3.21844C18.667 2.45219 17.7713 2 16.8189 2H7.18101C6.22864 2 5.33285 2.45219 4.76729 3.21844L1.84018 7.1842C1.04645 8.25957 1.05987 9.73033 1.87309 10.791L9.61913 20.8946C10.8199 22.4609 13.18 22.4609 14.3808 20.8946L22.1268 10.791C22.94 9.73033 22.9534 8.25957 22.1597 7.1842ZM3.72383 8L6.37644 4.40615C6.56496 4.15073 6.86355 4 7.18101 4H16.8189C17.1363 4 17.4349 4.15073 17.6235 4.40615L20.2761 8H3.72383ZM3.78676 10L11.2063 19.6777C11.6066 20.1998 12.3933 20.1998 12.7936 19.6777L20.2131 10H3.78676Z"
@@ -73,8 +94,17 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
             </svg>
 
             <div className="flex w-5/6 justify-between items-center">
-            <Link to='/products'>
-              <p onClick={changeLinkHandler} className={currentPath === 'products' ? "text-white" : "group-hover:text-white"}>Products</p>
+              <Link to="/products">
+                <p
+                  onClick={changeLinkHandler}
+                  className={
+                    currentPath === "products"
+                      ? "text-white"
+                      : "group-hover:text-white"
+                  }
+                >
+                  Products
+                </p>
               </Link>
               <div className="flex space-x-3 items-center">
                 {/* Add product button */}
@@ -122,7 +152,7 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
                 {/* Add product button */}
 
                 {/* open product sub menu button */}
-                <button
+                {/* <button
                   className={
                     productSubMenu
                       ? "rotate-180 transition-all duration-75"
@@ -143,24 +173,33 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
                       fill="#6F767E"
                     />
                   </svg>
-                </button>
+                </button> */}
                 {/* open product sub menu button */}
               </div>
             </div>
           </div>
-          
 
           {productSubMenu && (
             <div className="flex flex-col w-4/6 ml-9">
-              <Link to='/products/drafts'><p className="menu-item hover:text-white">Drafts</p></Link>
-              <Link to='/products/released'><p className="menu-item hover:text-white">Released</p></Link>
+              <Link to="/products/drafts">
+                <p className="menu-item hover:text-white">Drafts</p>
+              </Link>
+              <Link to="/products/released">
+                <p className="menu-item hover:text-white">Released</p>
+              </Link>
             </div>
           )}
         </aside>
 
         <aside>
-          
-          <div id="menu-item" className={currentPath === 'customers' ? "menu-item-active group" : 'menu-item group'}>
+          <div
+            id="menu-item"
+            className={
+              currentPath === "customers"
+                ? "menu-item-active group"
+                : "menu-item group"
+            }
+          >
             <svg
               width="24"
               height="24"
@@ -169,14 +208,22 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                className={currentPath === 'customers' ? "fill-white" : "group-hover:fill-white"}
+                className={
+                  currentPath === "customers"
+                    ? "fill-white"
+                    : "group-hover:fill-white"
+                }
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12ZM12 14C14.2091 14 16 12.2091 16 10C16 7.79086 14.2091 6 12 6C9.79086 6 8 7.79086 8 10C8 12.2091 9.79086 14 12 14Z"
                 fill="#6F767E"
               />
               <path
-                className={currentPath === 'customers' ? "fill-white" : "group-hover:fill-white"}
+                className={
+                  currentPath === "customers"
+                    ? "fill-white"
+                    : "group-hover:fill-white"
+                }
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M18.5588 19.5488C20.6672 17.7154 22 15.0134 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 15.0134 3.33284 17.7154 5.44116 19.5488C7.19693 21.0756 9.49052 22 12 22C14.4162 22 16.6323 21.143 18.3609 19.7165C18.4276 19.6614 18.4936 19.6055 18.5588 19.5488ZM12.2579 19.9959C12.1723 19.9986 12.0863 20 12 20C11.9914 20 11.9827 20 11.9741 20C11.8937 19.9997 11.8135 19.9983 11.7337 19.9956C10.3914 19.9517 9.13273 19.5772 8.03655 18.9508C8.95181 17.7632 10.3882 17 12 17C13.6118 17 15.0482 17.7632 15.9634 18.9508C14.865 19.5785 13.6033 19.9533 12.2579 19.9959ZM17.5624 17.7498C16.2832 16.0781 14.2675 15 12 15C9.73249 15 7.7168 16.0781 6.43759 17.7498C4.93447 16.2953 4 14.2568 4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 14.2568 19.0655 16.2953 17.5624 17.7498Z"
@@ -185,8 +232,17 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
             </svg>
 
             <div className="flex w-5/6 justify-between">
-            <Link to='/customers'>
-              <p onClick={changeLinkHandler} className={currentPath === 'customers' ? "text-white" : "group-hover:text-white"}>Customers</p>
+              <Link to="/customers">
+                <p
+                  onClick={changeLinkHandler}
+                  className={
+                    currentPath === "customers"
+                      ? "text-white"
+                      : "group-hover:text-white"
+                  }
+                >
+                  Customers
+                </p>
               </Link>
 
               {/* open customers sub menu button */}
@@ -215,18 +271,19 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
               {/* open customers sub menu button */}
             </div>
           </div>
-          
 
           {customerSubMenu && (
             <div className="flex flex-col w-4/6 ml-9">
-              <Link to='/customers/customer-list'><p className="menu-item hover:text-white">Customer list</p></Link>
+              <Link to="/customers/customer-list">
+                <p className="menu-item hover:text-white">Customer list</p>
+              </Link>
             </div>
           )}
         </aside>
 
-        <aside>
-          
-          <div id="menu-item"  className={currentPath === 'income' ? "menu-item-active group" : 'menu-item group'}>
+        {/* <aside> */}
+
+        {/* <div id="menu-item"  className={currentPath === 'income' ? "menu-item-active group" : 'menu-item group'}>
             <svg
               width="24"
               height="24"
@@ -254,7 +311,6 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
             <Link to='/income'>
               <p onClick={changeLinkHandler} className={currentPath === 'income' ? "text-white" : "group-hover:text-white"}>Income</p>
               </Link>
-              {/* open income sub menu button */}
               <button
                 className={
                   incomeSubMenu
@@ -277,17 +333,15 @@ export default function SidebarContent({ setMobileMenuOpen, currentPath}) {
                   />
                 </svg>
               </button>
-              {/* open income sub menu button */}
             </div>
-          </div>
-          
+          </div> */}
 
-          {incomeSubMenu && (
+        {/* {incomeSubMenu && (
             <div className="flex flex-col w-4/6 ml-9">
               <Link to='/income/payouts'><p className="menu-item hover:text-white">Payouts</p></Link>
             </div>
-          )}
-        </aside>
+          )} */}
+        {/* </aside> */}
       </div>
     </div>
   );
